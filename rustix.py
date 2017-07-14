f7d70abf694d35c870c5cc6c2a0ea9dd6f608571
Stefan Lau <github@stefanlau.com>
2017-07-14 19:53:20 +0200

    Refactor all the things

--- rustix.py
+++ rustix.py
@@ -1,47 +1,96 @@
 import argparse
 import os.path as path
 import pytoml as toml
+import textwrap
 
 
-def nix_derivation_name(name, version):
+def derivation_name(name, version):
     return "{}_{}".format(name.replace("-", "_"), version.replace('.', '_'))
 
+def dedent(str):
+    return textwrap.dedent(str).strip()
 
-def build_metadata_key(package):
+
+class CrateDerivation:
+    def __init__(self, package):
+        self.name = package['name']
+        self.version = package['version']
+        self.source = package['source'] if 'source' in package else None
+        self.dependencies = package.get('dependencies', [])
+        self.sha256 = package['sha256'] if 'sha256' in package else None
+
+    def derivation_name(self):
+        return derivation_name(self.name, self.version)
+
+    def render_src(self):
+        raise NotImplementedException('Render called for dependency with unknown type: {}', self.name)
+
+    def render_dependencies(self):
+        return "dependencies = [ {} ];".format(' '.join([
+            derivation_name(*d.split(' ')[:2]) for d in self.dependencies
+        ]))
+
+    def render(self):
+        return dedent("""
+            {derivation_name} = mkRustCrate {{
+                crateName = "{name}";
+                version = "{version}";
+                {dependencies}
+                {src}
+                inherit release;
+            }};
+        """).format(**{
+            'name': self.name,
+            'derivation_name': self.derivation_name(),
+            'version': self.version,
+            'dependencies': self.render_dependencies() if len(self.dependencies) > 0 else '',
+            'src': textwrap.indent(self.render_src(), '    ')
+        })
+
+
+class RootCrateDerivation(CrateDerivation):
+    def render_src(self):
+        return 'src = ./.;'
+
+
+class VersionedCrateDerivation(CrateDerivation):
+    def render_src(self):
+        return dedent("""
+            src = fetchurl {{
+                url = "https://crates.io/api/v1/crates/{name}/{version}/download";
+                sha256 = "{sha256}";
+                name = "{name}-{version}.tar.gz";
+            }};
+        """).format(name=self.name, version=self.version, sha256=self.sha256)
+
+
+def metadata_key(package):
     return "checksum {} {} ({})".format(package['name'], package['version'], package['source'])
 
+def dependency_derivation(package, metadata):
+    sha256 = metadata[metadata_key(package)]
+    return VersionedCrateDerivation({ **package ,'sha256': sha256})
 
-def dependency_to_derivation_name(dependency):
-    [name, version] = dependency.split(' ')[:2]
-    return nix_derivation_name(name, version)
 
+def render_nix_from_cargo_lock(lockfile):
+    metadata = lockfile['metadata']
 
-def make_package_derivation(metadata, package):
-    name = package['name']
-    version = package['version']
-    dependencies = [dependency_to_derivation_name(d) for d in package['dependencies']] if 'dependencies' in package else None
-    dependencies_string = "dependencies = [ {} ];".format(' '.join(dependencies)) if dependencies else ""
-    derivation = {
-        'name': name,
-        'version': version,
-        'derivation_name': nix_derivation_name(name, version),
-        'dependencies': dependencies_string, 
-        'sha256': metadata[build_metadata_key(package)], 
-    }
-    
-    return """
-    {derivation_name} = mkRustCrate {{
-        crateName = "{name}";
-        version = "{version}";
-        {dependencies}
-        src = fetchurl {{
-            url = "https://crates.io/api/v1/crates/{name}/{version}/download";
-            sha256 = "{sha256}";
-            name = "{name}-{version}.tar.gz";
-        }};
-        inherit release;
-    }};
-    """.format(**derivation)
+    root = RootCrateDerivation(lockfile['root'])
+    dependeny_derivations = [dependency_derivation(package, metadata) for package in lockfile['package']]
+    all_derivations_rendered = [d.render() for d in dependeny_derivations] + [ root.render() ]
+
+    return  dedent("""
+        {{ mkRustCrate, fetchurl }}:
+        let
+            release = true;
+        {derivations}
+        in
+            {root}
+    """).format(**{
+        'derivations': '\n'.join([ textwrap.indent(d, '    ') for d in all_derivations_rendered ]),
+        'root': root.derivation_name()
+    })
+
 
 argparser = argparse.ArgumentParser(description='Build nix derivations from Cargo.lock')
 argparser.add_argument('crate', help='Path to crate (where Cargo.toml and Cargo.lock are located')
@@ -50,29 +99,6 @@ args = argparser.parse_args()
 
 with open(path.realpath(path.join(args.crate, 'Cargo.lock')), 'rb') as lf:
     lockfile = toml.load(lf)
-
-    root = lockfile['root']
-    root['dependencies'] = ' '.join([dependency_to_derivation_name(d) for d in root['dependencies']])
-
-    dependencies_derivations = '\n'.join([
-        make_package_derivation(lockfile['metadata'], package) for package in lockfile['package']
-    ])
-    top_level_derivation = """
-{{ mkRustCrate, fetchurl }}:
-let
-    release = true;
-    {dependencies_derivations}
-in
-mkRustCrate {{
-    crateName = "{root[name]}";
-    version = "{root[version]}";
-    dependencies = [ {root[dependencies]} ];
-    src = ./.;
-    inherit release;
-}}
-    """.format(**{
-        'dependencies_derivations': dependencies_derivations,
-        'root': root
-    })
-    print(top_level_derivation)
+    rendered = render_nix_from_cargo_lock(lockfile)
+    print(rendered)
 
